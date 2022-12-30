1c3d7af556822b3a25af68d2742abff0c1f60800
Otabek <otabekrahmonovtest@gmail.com>
2022-12-30 09:28:30 +0500

    Urls

--- root/urls.py
+++ root/urls.py
@@ -9,6 +9,15 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     path('admin/', admin.site.urls),
 
+    # main app
+    path('api/v1/main/', include('main.urls')),
+
+    # worker app
+    path('api/v1/worker/', include('worker.urls')),
+    
+    # company app
+    path('api/v1/company/', include('company.urls')),
+
     # simple-jwt
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
